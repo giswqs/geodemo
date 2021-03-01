ae514a7c10766d327a659d659eb75ef0db608fca
Qiusheng Wu <giswqs@gmail.com>
2021-03-01 10:00:40 -0600

    Added functionality to package

--- geodemo/geodemo.py
+++ geodemo/geodemo.py
@@ -1,1 +1,57 @@
 """Main module."""
+
+import ipyleaflet
+from ipyleaflet import FullScreenControl, LayersControl, DrawControl, MeasureControl, ScaleControl, TileLayer
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, **kwargs):
+
+        if "center" not in kwargs:
+            kwargs["center"] = [40, -100]
+
+        if "zoom" not in kwargs:
+            kwargs["zoom"] = 4
+
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+
+        super().__init__(**kwargs)
+
+        if "height" not in kwargs:
+            self.layout.height = "600px"
+        else:
+            self.layout.height = kwargs["height"]
+
+        self.add_control(FullScreenControl())
+        self.add_control(LayersControl(position="topright"))
+        self.add_control(DrawControl(position="topleft"))
+        self.add_control(MeasureControl())
+        self.add_control(ScaleControl(position="bottomleft"))
+
+        if "google_map" not in kwargs:
+            layer = TileLayer(
+                url="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+                attribution="Google",
+                name="Google Maps",
+            )
+            self.add_layer(layer)
+        else:
+            if kwargs["google_map"] == "ROADMAP":
+                layer = TileLayer(
+                    url="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+                    attribution="Google",
+                    name="Google Maps",
+                )
+                self.add_layer(layer)
+            elif kwargs["google_map"] == "HYBRID":
+                layer = TileLayer(
+                    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+                    attribution="Google",
+                    name="Google Satellite"
+                )
+                self.add_layer(layer)
+
+
+
+
