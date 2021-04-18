328821ac7ecc2466d3b6602500ae72255256afbc
Qiusheng Wu <giswqs@gmail.com>
2021-04-18 15:16:10 -0400

    Added unittest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,10 +2,10 @@
 
 """Tests for `utils` package."""
 
-
+import os
 import unittest
 
-from geodemo import utils
+from geodemo import utils, geodemo
 
 
 class TestUtils(unittest.TestCase):
@@ -13,9 +13,49 @@ class TestUtils(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        print("setUp")
+        self.in_shp = os.path.abspath("examples/data/countries.shp")
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        print("tearDown\n")
 
-    def test_000_something(self):
-        """Test something."""
+    def test_random_string(self):
+        print("test_random_string")
+        self.assertEqual(len(utils.random_string(3)), 3)
+
+    def test_add(self):
+        print("test_add")
+        self.assertEqual(utils.add(-1, 1), 0)
+        self.assertEqual(utils.add(-1, -1), -2)
+        self.assertEqual(utils.add(10, 5), 15)
+
+    def test_subtract(self):
+        print("test_subtract")
+        self.assertEqual(utils.subtract(10, 5), 5)
+        self.assertEqual(utils.subtract(-1, 1), -2)
+        self.assertEqual(utils.subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        print("test_multiply")
+        self.assertEqual(utils.multiply(10, 5), 50)
+        self.assertEqual(utils.multiply(-1, 1), -1)
+        self.assertEqual(utils.multiply(-1, -1), 1)
+
+    def test_divide(self):
+        print("test_divide")
+        self.assertEqual(utils.divide(10, 5), 2)
+        self.assertEqual(utils.divide(-1, 1), -1)
+        self.assertEqual(utils.divide(-1, -1), 1)
+        self.assertEqual(utils.divide(5, 2), 2.5)
+
+        with self.assertRaises(ValueError):
+            utils.divide(10, 0)
+
+    def test_shp_to_geojson(self):
+        print("test_shp_to_geojson")
+        self.assertIsInstance(geodemo.shp_to_geojson(self.in_shp), dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
